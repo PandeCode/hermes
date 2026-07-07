2783686db4fdb143ec1665ff4ad4f95f9c18305d
PandeCode <pandeshawnbenjamin@gmail.com>
2026-07-07 12:44:49 -0400

    more hl

---
+++ queries/nix/injections.scm
@@ -0,0 +1,124 @@
+;; extends
+;; https://github.com/calops/hmts.nvim/
+
+; home.file.*.text
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "home" "file" ".*" "text")
+  expression: (_ (string_fragment) @injection.content)
+  (#hmts-inject! @_path)
+  (#set! injection.combined)
+)
+
+; xdg.configFile.*.text
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "xdg" "configFile" ".*" "text")
+  expression: (_ (string_fragment) @injection.content)
+  (#hmts-inject! @_path)
+  (#set! injection.combined)
+)
+
+; Strings with shebang expressions:
+;   ''
+;   #! /bin/lang
+;   ''
+(
+  (indented_string_expression
+    (string_fragment) @injection.language (#lua-match? @injection.language "^%s*#!")
+  ) @injection.content
+  (#gsub! @injection.language ".*#!.*env (%S+).*" "%1")
+  (#gsub! @injection.language ".*#!%s*%S*/(%S+).*" "%1")
+  (#set! injection.include-children)
+  (#set! injection.combined)
+)
+
+; Explicit annotations in comments:
+;   /* lang */ ''script''
+; or:
+;   # lang
+;   ''script''
+((comment) @injection.language
+  .
+  (_ (string_fragment) @injection.content)
+  (#gsub! @injection.language "[/*#%s]" "")
+  (#set! injection.combined)
+)
+
+; Fish
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "programs" "fish" "((interactive|login)S|s)hellInit$")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "fish")
+  (#set! injection.combined)
+)
+
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "programs" "fish" "(shellAliases|shellAbbrs|functions)" ".*" "body")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "fish")
+  (#set! injection.combined)
+)
+
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "programs" "fish" "(shellAliases|shellAbbrs|functions)" ".*")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "fish")
+  (#set! injection.combined)
+)
+
+; Bash
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "programs" "bash" "(init|logout|profile|bashrc)Extra$")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "bash")
+  (#set! injection.combined)
+)
+
+; Zsh
+(binding
+  attrpath: (_) @_path
+  (#hmts-path? @_path "programs" "zsh" "(completionInit|envExtra|initContent|loginExtra|logoutExtra|profileExtra)$")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "bash")
+  (#set! injection.combined)
+)
+
+; oh-my-zsh
+(binding
+  attrpath: (_) @_path
+  (#hmts-path? @_path "programs" "zsh" "oh-my-zsh" "extraConfig$")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "bash")
+  (#set! injection.combined)
+)
+
+; Firefox
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "programs" "firefox" "profiles" ".*" "userChrome")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "css")
+  (#set! injection.combined)
+)
+
+; Polkit
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "security" "polkit" "extraConfig")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "javascript")
+  (#set! injection.combined)
+)
+
+; Wezterm
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "programs" "wezterm" "extraConfig")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "lua")
+  (#set! injection.combined)
+)
+
+; Waybar
+(binding
+  attrpath: (_) @_path (#hmts-path? @_path "programs" "waybar" "style")
+  expression: (_ (string_fragment) @injection.content)
+  (#set! injection.language "css")
+  (#set! injection.combined)
+)
--- queries/zig/injections.scm
+++ queries/zig/injections.scm
@@ -1,44 +1,10 @@
-; C
-(
-  (comment) @comment
-  .
-  (multiline_string) @injection.content
-  (#match? @comment "^//[ \t]*c$")
-  (#set! injection.language "c")
-)
+;; extends
 
-; JSON
 (
-  (comment) @comment
+  (comment) @injection.language
   .
   (multiline_string) @injection.content
-  (#match? @comment "^//[ \t]*json$")
-  (#set! injection.language "json")
-)
-
-; ASM
-(
-  (comment) @comment
-  .
-  (multiline_string) @injection.content
-  (#match? @comment "^//[ \t]*asm$")
-  (#set! injection.language "asm")
-)
-
-; GLSL
-(
-  (comment) @comment
-  .
-  (multiline_string) @injection.content
-  (#match? @comment "^//[ \t]*glsl$")
-  (#set! injection.language "glsl")
-)
-
-; WGSL
-(
-  (comment) @comment
-  .
-  (multiline_string) @injection.content
-  (#match? @comment "^//[ \t]*wgsl$")
-  (#set! injection.language "wgsl")
+  (#match? @injection.language "^//[ \t]*[A-Za-z0-9_+-]+$")
+  (#gsub! @injection.language "^//[ \t]*" "")
+  (#set! injection.combined)
 )
